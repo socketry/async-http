0fec5d0435b5d81a9a7f1ae393c8a84b597b6347
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2018-08-09 21:17:53 +0900

    Use Headers from `http-protocol` gem.

--- lib/async/http/headers.rb
+++ lib/async/http/headers.rb
@@ -18,6 +18,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+require 'http/protocol/headers'
+
 module Async
 	module HTTP
 		Headers = ::HTTP::Protocol::Headers

de3c73088d31b797c0dcab46f881ae8d0af03507
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-10-16 00:59:03 +1300

    Add more terms.

--- examples/google/search.rb
+++ examples/google/search.rb
@@ -12,9 +12,11 @@ class Google < Protocol::HTTP::Middleware
 	def search(term)
 		Async.logger.info(self) {"Searching for #{term}..."}
 		
-		self.get("/search?q=#{term}")
+		self.get("/search?q=#{term}", {"user-agent" => "Hi Google!"})
 	end
 end
+
+TERMS = %w{thoughtful fear size payment lethal modern recognise face morning sulky mountainous contain science snow uncle skirt truthful door travel snails closed rotten halting creator teeny-tiny beautiful cherries unruly level follow strip team things suggest pretty warm end cannon bad pig consider airport strengthen youthful fog three walk furry pickle moaning fax book ruddy sigh plate cakes shame stem faulty bushes dislike train sleet one colour behavior bitter suit count loutish squeak learn watery orange idiotic seat wholesale omniscient nostalgic arithmetic instruct committee puffy program cream cake whistle rely encourage war flagrant amusing fluffy prick utter wacky occur daily son check}
 
 Async do |task|
 	client = Async::HTTP::Client.new(ENDPOINT)
@@ -22,10 +24,8 @@ Async do |task|
 	
 	google.search("null").finish
 	
-	terms = %w{cats dogs chickens pigs fish horse goat cow sheep mice alpaca pigeon tui kea weka}
-	
 	duration = Async::Clock.measure do
-		counts = terms.map do |term|
+		counts = TERMS.map do |term|
 			task.async do
 				response = google.search(term)
 				[term, response.read.scan(term).count]

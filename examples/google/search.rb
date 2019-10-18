8c5858febd3b824fd4808d0e8edc7e9efc1bbb40
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-10-19 02:03:10 +1300

    Improvements to Google DoS script...

--- examples/google/search.rb
+++ examples/google/search.rb
@@ -3,7 +3,7 @@
 require "async"
 require "async/clock"
 require "protocol/http/middleware"
-require "../../lib/async/http"
+require_relative "../../lib/async/http"
 
 URL = "https://www.google.com/search"
 ENDPOINT = Async::HTTP::Endpoint.parse(URL)
@@ -16,7 +16,11 @@ class Google < Protocol::HTTP::Middleware
 	end
 end
 
-TERMS = %w{thoughtful fear size payment lethal modern recognise face morning sulky mountainous contain science snow uncle skirt truthful door travel snails closed rotten halting creator teeny-tiny beautiful cherries unruly level follow strip team things suggest pretty warm end cannon bad pig consider airport strengthen youthful fog three walk furry pickle moaning fax book ruddy sigh plate cakes shame stem faulty bushes dislike train sleet one colour behavior bitter suit count loutish squeak learn watery orange idiotic seat wholesale omniscient nostalgic arithmetic instruct committee puffy program cream cake whistle rely encourage war flagrant amusing fluffy prick utter wacky occur daily son check}
+terms = %w{thoughtful fear size payment lethal modern recognise face morning sulky mountainous contain science snow uncle skirt truthful door travel snails closed rotten halting creator teeny-tiny beautiful cherries unruly level follow strip team things suggest pretty warm end cannon bad pig consider airport strengthen youthful fog three walk furry pickle moaning fax book ruddy sigh plate cakes shame stem faulty bushes dislike train sleet one colour behavior bitter suit count loutish squeak learn watery orange idiotic seat wholesale omniscient nostalgic arithmetic instruct committee puffy program cream cake whistle rely encourage war flagrant amusing fluffy prick utter wacky occur daily son check}
+
+if count = ENV['COUNT']&.to_i
+	terms = terms.first(count)
+end
 
 Async do |task|
 	client = Async::HTTP::Client.new(ENDPOINT)
@@ -25,7 +29,7 @@ Async do |task|
 	google.search("null").finish
 	
 	duration = Async::Clock.measure do
-		counts = TERMS.map do |term|
+		counts = terms.map do |term|
 			task.async do
 				response = google.search(term)
 				[term, response.read.scan(term).count]

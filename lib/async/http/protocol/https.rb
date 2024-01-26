342333abca245d657ca024cc54bd5684376be898
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-01-26 18:25:17 +1300

    Remove unnecessary alpn hack.

--- lib/async/http/protocol/https.rb
+++ lib/async/http/protocol/https.rb
@@ -9,24 +9,6 @@ require_relative 'http11'
 
 require_relative 'http2'
 
-require 'openssl'
-
-unless OpenSSL::SSL::SSLContext.instance_methods.include? :alpn_protocols=
-	warn "OpenSSL implementation doesn't support ALPN."
-	
-	class OpenSSL::SSL::SSLContext
-		def alpn_protocols= names
-			return names
-		end
-	end
-	
-	class OpenSSL::SSL::SSLSocket
-		def alpn_protocol
-			return nil
-		end
-	end
-end
-
 module Async
 	module HTTP
 		module Protocol

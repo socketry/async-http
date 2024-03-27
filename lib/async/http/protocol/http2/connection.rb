1d3f1215b1f6b1465afed263eafefdf692f351c8
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-03-28 11:47:50 +1300

    We don't need to defer stop in the HTTP/2 background reader.

--- lib/async/http/protocol/http2/connection.rb
+++ lib/async/http/protocol/http2/connection.rb
@@ -89,7 +89,8 @@ module Async
 							
 							task.annotate("#{version} reading data for #{self.class}.")
 							
-							task.defer_stop do
+							# We don't need to defer stop here as this is already a transient task (ignores stop):
+							begin
 								while !self.closed?
 									self.consume_window
 									self.read_frame

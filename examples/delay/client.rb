53d5b4fc1117d9b9f3edecc467557c2e9489de93
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-06-10 14:58:14 +0900

    Add delay example.

---
+++ examples/delay/client.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+require "net/http"
+require "async"
+require "async/http/internet"
+require "async/barrier"
+require "async/semaphore"
+
+N_TIMES = 1000
+
+Async do |task|
+	internet = Async::HTTP::Internet.new
+	barrier = Async::Barrier.new
+	
+	results = N_TIMES.times.map do |i|
+		barrier.async do
+			puts "Run #{i}"
+			
+			begin
+				response = internet.get("https://httpbin.org/delay/0.5")
+			ensure
+				response&.finish
+			end
+		end
+	end
+	
+	barrier.wait
+end

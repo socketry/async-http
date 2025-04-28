391218c0ee164750202df62bb3bb097ad586ec01
Jean Boussier <jean.boussier@gmail.com>
2025-04-28 10:08:58 +0200

    Fix unused variable warnings
    ```
    async-http-0.86.0/lib/async/http/protocol/http1/server.rb:55: warning: assigned but unused variable - error
    async-http-0.86.0/lib/async/http/protocol/http2/connection.rb:99: warning: assigned but unused variable - ignored_error
    ```

--- lib/async/http/protocol/http1/server.rb
+++ lib/async/http/protocol/http1/server.rb
@@ -52,7 +52,7 @@ module Async
 						end
 						
 						return request
-					rescue ::Protocol::HTTP1::BadRequest => error
+					rescue ::Protocol::HTTP1::BadRequest
 						fail_request(400)
 						# Conceivably we could retry here, but we don't really know how bad the error is, so it's better to just fail:
 						raise

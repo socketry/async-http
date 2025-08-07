ff8c30126bef588593ac6e976af15c883ebb127e
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-08-07 15:10:53 +0800

    Rename `completed` -> `finished` as it can also be for errors.

--- lib/metrics/provider/async/http/client.rb
+++ lib/metrics/provider/async/http/client.rb
@@ -8,19 +8,19 @@ require "metrics/provider"
 
 Metrics::Provider(Async::HTTP::Client) do
 	ASYNC_HTTP_CLIENT_REQUEST_INITIATED = Metrics.metric("async.http.client.request.initiated", :counter, description: "The number of HTTP client requests initiated.")
-	ASYNC_HTTP_CLIENT_REQUEST_COMPLETED = Metrics.metric("async.http.client.request.completed", :counter, description: "The number of HTTP client requests completed.")
-	
+	ASYNC_HTTP_CLIENT_REQUEST_FINISHED = Metrics.metric("async.http.client.request.finished", :counter, description: "The number of HTTP client requests finished.")
+
 	def make_response(request, connection, attempt)
 		ASYNC_HTTP_CLIENT_REQUEST_INITIATED.emit(1, tags: ["method:#{request.method}"])
 		
 		response = super(request, connection, attempt)
 		
-		ASYNC_HTTP_CLIENT_REQUEST_COMPLETED.emit(1, tags: ["method:#{request.method}", "status:#{response.status}"])
+		ASYNC_HTTP_CLIENT_REQUEST_FINISHED.emit(1, tags: ["method:#{request.method}", "status:#{response.status}"])
 		
 		return response
 	ensure
 		unless response
-			ASYNC_HTTP_CLIENT_REQUEST_COMPLETED.emit(1, tags: ["method:#{request.method}", "status:failed"])
+			ASYNC_HTTP_CLIENT_REQUEST_FINISHED.emit(1, tags: ["method:#{request.method}", "status:failed"])
 		end
 	end
 end
--- lib/metrics/provider/async/http/server.rb
+++ lib/metrics/provider/async/http/server.rb
@@ -8,20 +8,20 @@ require "metrics/provider"
 
 Metrics::Provider(Async::HTTP::Server) do
 	ASYNC_HTTP_SERVER_REQUEST_INITIATED = Metrics.metric("async.http.server.request.initiated", :counter, description: "The number of HTTP server requests initiated.")
-	ASYNC_HTTP_SERVER_REQUEST_COMPLETED = Metrics.metric("async.http.server.request.completed", :counter, description: "The number of HTTP server requests completed.")
+	ASYNC_HTTP_SERVER_REQUEST_FINISHED = Metrics.metric("async.http.server.request.finished", :counter, description: "The number of HTTP server requests finished.")
 
 	def call(request)
 		ASYNC_HTTP_SERVER_REQUEST_INITIATED.emit(1, tags: ["method:#{request.method}"])
 		
 		if response = super(request)
-			ASYNC_HTTP_SERVER_REQUEST_COMPLETED.emit(1, tags: ["method:#{request.method}", "status:#{response.status}"])
+			ASYNC_HTTP_SERVER_REQUEST_FINISHED.emit(1, tags: ["method:#{request.method}", "status:#{response.status}"])
 			return response
 		else
 			return nil
 		end
 	ensure
 		unless response
-			ASYNC_HTTP_SERVER_REQUEST_COMPLETED.emit(1, tags: ["method:#{request.method}", "status:failed"])
+			ASYNC_HTTP_SERVER_REQUEST_FINISHED.emit(1, tags: ["method:#{request.method}", "status:failed"])
 		end
 	end
 end

1c33ab0e4e75a3eec1a2a869582a6afe0a564f6d
Samuel Williams <samuel.williams@shopify.com>
2025-10-01 13:54:49 +1300

    Fix tests to use ServerContext fixture
    Use Sus::Fixtures::Async::HTTP::ServerContext which provides automatic
    server setup and a pre-configured client, instead of manually managing
    server tasks and endpoints.

--- test/metrics/provider/async/http/server.rb
+++ test/metrics/provider/async/http/server.rb
@@ -4,16 +4,11 @@
 # Copyright, 2025, by Samuel Williams.
 
 require "metrics/provider/async/http/server"
-require "async/http/server"
-require "async/http/endpoint"
-require "async/http/client"
 require "protocol/http/middleware"
-require "sus/fixtures/async"
+require "sus/fixtures/async/http/server_context"
 
 describe Async::HTTP::Server do
-	include Sus::Fixtures::Async::ReactorContext
-	
-	let(:endpoint) {Async::HTTP::Endpoint.parse("http://localhost:0")}
+	include Sus::Fixtures::Async::HTTP::ServerContext
 	
 	with "metrics provider" do
 		let(:app) do
@@ -22,13 +17,7 @@ describe Async::HTTP::Server do
 			end
 		end
 		
-		let(:server) {subject.new(app, endpoint)}
-		
 		it "emits queue time metric when x-request-start header is present" do
-			# Start the server
-			server_task = server.run
-			bound_endpoint = server_task.wait_until_ready
-			
 			# Calculate a timestamp 100ms in the past (nginx format with 't=' prefix)
 			request_start = Process.clock_gettime(Process::CLOCK_REALTIME) - 0.1
 			
@@ -39,21 +28,14 @@ describe Async::HTTP::Server do
 			end
 			
 			# Make a request with the x-request-start header
-			client = Async::HTTP::Client.new(bound_endpoint)
 			headers = [["x-request-start", "t=#{request_start}"]]
 			response = client.get("/", headers)
 			
 			expect(response.status).to be == 200
 			response.finish
-			
-			client.close
-			server_task.stop
 		end
 		
 		it "handles nginx-style timestamp format (t=prefix)" do
-			server_task = server.run
-			bound_endpoint = server_task.wait_until_ready
-			
 			request_start = Process.clock_gettime(Process::CLOCK_REALTIME) - 0.05
 			
 			expect(Async::HTTP::Server::ASYNC_HTTP_SERVER_REQUEST_QUEUE_TIME).to receive(:emit) do |value, tags:|
@@ -61,21 +43,14 @@ describe Async::HTTP::Server do
 				expect(value).to be < 1
 			end
 			
-			client = Async::HTTP::Client.new(bound_endpoint)
 			headers = [["x-request-start", "t=#{request_start}"]]
 			response = client.get("/", headers)
 			
 			expect(response.status).to be == 200
 			response.finish
-			
-			client.close
-			server_task.stop
 		end
 		
 		it "handles plain Unix timestamp format" do
-			server_task = server.run
-			bound_endpoint = server_task.wait_until_ready
-			
 			request_start = Process.clock_gettime(Process::CLOCK_REALTIME) - 0.05
 			
 			expect(Async::HTTP::Server::ASYNC_HTTP_SERVER_REQUEST_QUEUE_TIME).to receive(:emit) do |value, tags:|
@@ -83,52 +58,33 @@ describe Async::HTTP::Server do
 				expect(value).to be < 1
 			end
 			
-			client = Async::HTTP::Client.new(bound_endpoint)
 			headers = [["x-request-start", request_start.to_s]]
 			response = client.get("/", headers)
 			
 			expect(response.status).to be == 200
 			response.finish
-			
-			client.close
-			server_task.stop
 		end
 		
 		it "does not emit queue time metric when x-request-start header is missing" do
-			server_task = server.run
-			bound_endpoint = server_task.wait_until_ready
-			
 			# Should not emit the queue time metric
 			expect(Async::HTTP::Server::ASYNC_HTTP_SERVER_REQUEST_QUEUE_TIME).not.to receive(:emit)
 			
-			client = Async::HTTP::Client.new(bound_endpoint)
 			response = client.get("/")
 			
 			expect(response.status).to be == 200
 			response.finish
-			
-			client.close
-			server_task.stop
 		end
 		
 		it "ignores invalid timestamp formats" do
-			server_task = server.run
-			bound_endpoint = server_task.wait_until_ready
-			
 			# Should not emit the queue time metric for invalid timestamp
 			expect(Async::HTTP::Server::ASYNC_HTTP_SERVER_REQUEST_QUEUE_TIME).not.to receive(:emit)
 			
-			client = Async::HTTP::Client.new(bound_endpoint)
 			headers = [["x-request-start", "invalid-timestamp"]]
 			response = client.get("/", headers)
 			
 			expect(response.status).to be == 200
 			response.finish
-			
-			client.close
-			server_task.stop
 		end
-		
 	end
 end
 
